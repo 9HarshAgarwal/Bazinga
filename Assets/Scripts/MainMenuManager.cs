5fd3cff4a8ab5f22557dc0788c2e7d8f21c67a9f
Harsh-Gamestacy <ultramarinebicycle@gmail.com>
2023-04-16 15:09:33 +0530

    Create Main Menu scene basic components

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button instructionsButton;
+    [SerializeField] private Button exitButton;
+    [SerializeField] private TMP_Text highScoreText;
+    
+    [Header("Settings")]
+    [SerializeField] private string gameSceneName;
+    
+    private void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        instructionsButton.onClick.AddListener(PlayInstructions);
+        exitButton.onClick.AddListener(ExitGame);
+        highScoreText.text = $"High Score: {PlayerPrefs.GetInt("HighScore", 0)}";
+    }
+    
+    private void StartGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    private void PlayInstructions()
+    {
+    }
+    
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
